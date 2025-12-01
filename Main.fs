51dd534a36198456a9924e5c5b9e85a546922c99
Michael Nestler <194211291+phactum-mnestler@users.noreply.github.com>
2025-12-01 16:25:46 +0100

    Day 1

---
+++ Main.fs
@@ -0,0 +1,22 @@
+﻿namespace AOC2025
+
+open System
+open System.Text.Json
+
+module Program =
+    let solutions = Map [
+        "01", (AOC01.solveA, AOC01.solveB)
+    ]
+
+    [<EntryPoint>]
+    let main args =
+        printfn "Running example-%s" args[0]
+        let solveA, solveB = solutions[args[0]]
+        let solve = if args[1] = "a" then solveA else solveB
+        let exampleLines = System.IO.File.ReadAllLines($"inputs/example-{args[0]}.txt")
+        printfn "Result: %s" (solve exampleLines)
+        Console.ReadKey(true) |> ignore
+        printfn "Running inputs-%s" args[0]
+        let inputLines = System.IO.File.ReadAllLines($"inputs/input-{args[0]}.txt")
+        printfn "Result: %s" (solve inputLines)
+        0 // return an integer exit code
---
+++ aoc01.fs
@@ -0,0 +1,38 @@
+namespace AOC2025
+
+module AOC01 =
+
+    let dialSize = 100
+
+    let parse (expr: string) = 
+        if expr.StartsWith("R") 
+        then int(expr.Substring(1)) 
+        else -int(expr.Substring(1))
+    
+
+    let norm x d =
+        (x + d) % d
+
+    let solveA (lines: string array) = 
+        lines
+            |> Array.map parse
+            |> Array.scan (fun sum next -> norm (sum + next) dialSize) 50
+            |> Array.filter (fun dial -> dial = 0)
+            |> Array.length
+            |> string
+
+    let points (before, after) =
+        if after > before 
+        then int(floor (float(after) / 100.0) - floor (float(before) / 100.0))
+        else if before > after 
+        then int(floor ((float(before) - 1.0) / 100.0) - floor ((float(after) - 1.0) / 100.0))
+        else 0
+
+    let solveB (lines: string array) = 
+        lines
+            |> Array.map parse
+            |> Array.scan (fun sum next -> sum + next) 50
+            |> Array.pairwise
+            |> Array.map points
+            |> Array.sum
+            |> string
