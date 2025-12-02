9fd57688f933ce3c7647d92b1b159961239c34c7
Michael Nestler <194211291+phactum-mnestler@users.noreply.github.com>
2025-12-02 23:04:46 +0100

    Format using fantomas

--- Main.fs
+++ Main.fs
@@ -4,10 +4,8 @@ open System
 open System.Text.Json
 
 module Program =
-    let solutions = Map [
-        "01", (AOC01.solveA, AOC01.solveB)
-        "02", (AOC02.solveA, AOC02.solveB)
-    ]
+    let solutions =
+        Map [ "01", (AOC01.solveA, AOC01.solveB); "02", (AOC02.solveA, AOC02.solveB) ]
 
     [<EntryPoint>]
     let main args =
@@ -20,4 +18,4 @@ module Program =
         printfn "Running inputs-%s" args[0]
         let inputLines = System.IO.File.ReadAllLines($"inputs/input-{args[0]}.txt")
         printfn "Result: %s" (solve inputLines)
-        0 // return an integer exit code
+        0
